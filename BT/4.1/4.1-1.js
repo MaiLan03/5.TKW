b29933f5b43d4602d1950cc47b58cb8afec3b693
MaiLan03 <mailannguyen0812@gmail.com>
2024-11-21 16:32:02 +0700

    1632_211124

--- BT/4.1/4.1-1.js
+++ BT/4.1/4.1-1.js
@@ -1,11 +1,21 @@
-//bai1
-let number=-5;
-if (number<0){
-    console.log(number+" la so am");
-}
-else if(number>0){
-    console.log(number+" la so duong");
-}
-else{
-    console.log(number+" la so 0");
-}
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits.push("Kiwi");
+// console.log(fruits);
+
+// const name = "W3Schools";
+// let letter = name.at(2);
+// let x = name.at(12);
+// // let y = name.chatAt(-2);
+// console.log(letter);
+// console.log(x);
+// // console.log(y);
+
+
+const m = [1, 2, 3]
+m[1] = 5
+m.push(10)
+console.log("So phan tu: " + m.length)
+let s = 0
+for (let i = 0; i < m.length; i++)
+    s += m[i]
+console.log("Tong = " + s)
---
+++ C4.1/bai4.1-1.js
@@ -0,0 +1,11 @@
+//bai1
+let number=-5;
+if (number<0){
+    console.log(number+" la so am");
+}
+else if(number>0){
+    console.log(number+" la so duong");
+}
+else{
+    console.log(number+" la so 0");
+}
